192c7ea4cdbd1b9b0aba48ce5dc22f60dd913e51
vnk8071 <nguyenkhoi8071@gmail.com>
2022-11-16 12:30:09 +0700

    Update config and readme

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -3,7 +3,7 @@ echo 'Downloading and setting up model'
 DEST_DIR='.' 
 FILENAME='anomaly-detection-models.zip'
 
-gdown https://drive.google.com/uc?id=1aHNo7zaqFRUlqJcEH77LsMrzhJotOpb9
+gdown https://drive.google.com/uc?id=1VFjZz0HmsupRg9KSGRJAQTLhF4ZPQUK1
 mv $FILENAME $DEST_DIR
 unzip "${DEST_DIR}/${FILENAME}" -d $DEST_DIR
 rm "${DEST_DIR}/${FILENAME}"
