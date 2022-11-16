7007f7c6561c61b343f4ace063bf8a146fcc3e21
vnk8071 <nguyenkhoi8071@gmail.com>
2022-11-16 12:53:37 +0700

    Update requirement and debug=false

--- app.py
+++ app.py
@@ -180,4 +180,4 @@ def document():
     return render_template("document.html", name=current_user.name)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)   
+    app.run(host="0.0.0.0", port="5000", debug=False)   
